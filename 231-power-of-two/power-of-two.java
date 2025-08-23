650182303b1462e1f8ea8346f126e43ca71b1436
Radhika Rajagoli <rajgoliradhika111@gmail.com>
2025-08-23 13:48:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (5.67%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        int count= Integer.bitCount(n);
+        if(count==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
